e9fb394ddfc0544e0f1752cb2067d99594d17c10
ivine369 <ivinekemuma014@gmail.com>
2020-07-19 17:06:16 +0300

    add email

---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='4438'
+export MAIL_USERNAME='ivine09@gmail.com'
+export MAIL_PASSWORD='4438'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:123@localhost/pitchy'
+python3.6 manage.py server
