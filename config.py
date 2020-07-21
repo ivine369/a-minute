5141db281f4a61df293d1b13f669106bc0ff64ac
ivine369 <ivinekemuma014@gmail.com>
2020-07-21 13:49:10 +0300

    make changes

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ class Config:
 
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ivine:anne2001@localhost/pitching'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitching'
 
     SECRET_KEY = "try harder"
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 586
+    MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='4438'
 export MAIL_USERNAME='ivine09@gmail.com'
 export MAIL_PASSWORD='4438'
-export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://ivine:anne2001@localhost/pitching'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:Access@localhost/pitching'
 python3.6 manage.py server
